d1623ef9b94cdd22d4900f10d291f426f4e51df7
Hisham MG <hishamaziz.101@gmail.com>
2024-01-17 00:48:41 +0300

    initial

---
+++ src/main/java/org/ember/gearbox/GearboxApplication.java
@@ -0,0 +1,13 @@
+package org.ember.gearbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ember/gearbox/GearboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ember.gearbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
